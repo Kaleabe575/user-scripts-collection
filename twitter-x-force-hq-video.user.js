9b78fe116e69cd6085c6a7eab41f043396d6b427
Kaleabe Getnet <74538080+Kaleabe575@users.noreply.github.com>
2025-05-23 10:04:51 +0000

    bug fix

--- twitter-x-force-hq-video.user.js
+++ twitter-x-force-hq-video.user.js
@@ -66,7 +66,6 @@
         let userPaused = false;
         // Detect manual pause
         video.addEventListener('pause', function(e) {
-            // If the pause was triggered by user (not programmatically)
             if (!video.dataset.autoPaused && !video.dataset.outOfView) {
                 userPaused = true;
             }
@@ -102,6 +101,13 @@
             observer.observe(video);
             video.dataset.observing = '1';
         }
+        // Always set HQ and resume if needed when user clicks play
+        video.addEventListener('play', function() {
+            setHQ(video);
+            if (forceHQ && !userPaused && !video.dataset.outOfView && video.paused) {
+                video.play();
+            }
+        }, true);
     }
 
     // Observe for new videos
